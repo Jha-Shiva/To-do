c4c5ba0ae43893804ad19071f4d90c676a0c69d2
Jha-Shiva <shivamjha625@gmail.com>
2025-12-29 12:26:34 +0530

    user and task model completed

---
+++ Backend/models/task.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import User from "./user.model";
+
+const taskSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    title : {
+        type : String,
+        required: true,
+        trim : true
+    },
+    task : [
+        {
+            text : {
+                type : String,
+                required : true
+            },
+            isCompleted : {
+                type : Boolean,
+                default : false
+            }
+        }
+    ]
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
